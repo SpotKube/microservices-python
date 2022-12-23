07bd0a99d27b494df31668277a065a3921a40ce8
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2022-12-24 00:50:31 +0530

    Change name

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -6,11 +6,18 @@ from auth_svc import access
 from storage import util
 from bson.objectid import ObjectId
 
+
+mongo_user = os.environ.get("MONGO_USER")
+mongo_password = os.environ.get("MONGO_PASSWORD")
+
+uri_video = f"mongodb://{mongo_user}:{mongo_password}@host.minikube.internal:27017/videos"
+uri_mp3 = f"mongodb://{mongo_user}:{mongo_password}@host.minikube.internal:27017/mp3s"
+
 server = Flask(__name__)
 
-mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+mongo_video = PyMongo(server, uri=uri_video)
 
-mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+mongo_mp3 = PyMongo(server, uri=uri_mp3)
 
 fs_videos = gridfs.GridFS(mongo_video.db)
 fs_mp3s = gridfs.GridFS(mongo_mp3.db)
